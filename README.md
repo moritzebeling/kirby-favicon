0582eedf12ceb3f4c3676c31cff3c1955e9c065d
moritzebeling <git@moritzebeling.com>
2023-02-15 13:07:30 +0100

    plugin options

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Kirby Favicon
+
+## Options
+
+```php
+return [
+    'moritzebeling.kirby-favicon' => [
+        'directory' => 'assets/favicon',
+        'sizes' => [
+            16,
+            32,
+            48,
+            64,
+            96,
+            128,
+            144,
+            180,
+            192,
+            256,
+            512,
+            1024
+        ],
+        'color' => '#0000ff',
+        'background' => '#000000',
+    ]
+];
+```
--- index.php
+++ index.php
@@ -2,4 +2,26 @@
 
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin('moritzebeling/kirby-favicon');
+Kirby::plugin('moritzebeling/kirby-favicon',[
+
+    'options' => [
+        'directory' => 'assets/favicon',
+        'sizes' => [
+            16,
+            32,
+            48,
+            64,
+            96,
+            128,
+            144,
+            180,
+            192,
+            256,
+            512,
+            1024
+        ],
+        'color' => '#0000ff',
+        'background' => '#000000',
+    ]
+
+]);
