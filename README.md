73a33eec0b8d12365d4ce489244d1756d7a8e7ff
moritzebeling <git@moritzebeling.com>
2023-02-19 15:03:33 +0100

    manifest.json

--- README.md
+++ README.md
@@ -5,23 +5,42 @@
 ```php
 return [
     'moritzebeling.kirby-favicon' => [
-        'icons' => [
+        'color' => '#0000ff',
+        'favicon' => [
             'png' => 'assets/favicon/favicon.png', // required
             'ico' => 'assets/favicon/favicon.ico',
             'svg' => 'assets/favicon/favicon.svg',
-            'app-icon' => 'assets/favicon/app-icon.png',
-            'ms-tile' => 'assets/favicon/ms-tile.png',
+            'sizes' => [ 32, 96, 16 ],
         ],
-        'sizes' => [
-            32,
-            96,
-            16
+        'manifest' => [
+            'icon' => 'assets/favicon/android-icon.png',
+            'background_color' => '#000000',
+            'sizes' => [
+                36 => 0.75,
+                48 => 1.0,
+                72 => 1.5,
+                96 => 2.0,
+                144 => 3.0,
+                192 => 4.0,
+                512 => false
+            ],
+            // other entries can be added here
         ],
-        'color' => '#0000ff',
-        'background' => '#000000',
+        'browserconfig' => [
+            'icon' => 'assets/favicon/ms-tile.png',
+            'sizes' => [
+                70,
+                150,
+                310
+            ]
+        ],
     ]
 ];
 ```
+
+## manifest.json
+
+If you don’t provide a specific icon at `assets/favicon/android-icon.png`, it will fall back to the `png` version. You can add other values according to the [specification](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json).
 
 ## browserconfig.xml
 
--- index.php
+++ index.php
@@ -7,20 +7,35 @@ use Kirby\Cms\Response;
 Kirby::plugin('moritzebeling/kirby-favicon',[
 
     'options' => [
-        'icons' => [
+        'color' => '#0000ff',
+        'favicon' => [
             'png' => 'assets/favicon/favicon.png', // required
             'ico' => 'assets/favicon/favicon.ico',
             'svg' => 'assets/favicon/favicon.svg',
-            'app-icon' => 'assets/favicon/app-icon.png',
-            'ms-tile' => 'assets/favicon/ms-tile.png',
+            'sizes' => [ 32, 96, 16 ],
         ],
-        'sizes' => [
-            32,
-            96,
-            16
+        'manifest' => [
+            'icon' => 'assets/favicon/android-icon.png',
+            'background_color' => '#000000',
+            'sizes' => [
+                36 => 0.75,
+                48 => 1.0,
+                72 => 1.5,
+                96 => 2.0,
+                144 => 3.0,
+                192 => 4.0,
+                512 => false
+            ],
+            // other entries can be added here
         ],
-        'color' => '#0000ff',
-        'background' => '#000000',
+        'browserconfig' => [
+            'icon' => 'assets/favicon/ms-tile.png',
+            'sizes' => [
+                70,
+                150,
+                310
+            ]
+        ],
     ],
 
     'snippets' => [
@@ -36,23 +51,62 @@ Kirby::plugin('moritzebeling/kirby-favicon',[
             }
         ],
         [
+            'pattern' => 'manifest.json',
+            'action'  => function () {
+
+                $icons = [
+                    option('moritzebeling.kirby-favicon.manifest.icon'),
+                    option('moritzebeling.kirby-favicon.favicon.png')
+                ];
+                foreach( $icons as $i ){
+                    $icon = asset( $i );
+                    if( $icon->exists() ) break;
+                }
+
+                $json = array_merge([
+                    'name' => (string)kirby()->site()->title(),
+                    'theme_color' => option('moritzebeling.kirby-favicon.color'),
+                    'display' => 'standalone',
+                    'icons' => []
+                ],option('moritzebeling.kirby-favicon.manifest', []));
+
+                $sizes = $icon->exists() ? option('moritzebeling.kirby-favicon.manifest.sizes', []) : [];
+
+                foreach( $sizes as $size => $density ){
+                    $json['icons'][] = [
+                        'src' => $icon->resize($size)->url(),
+                        'sizes' => $size . 'x' . $size,
+                        'type' => 'image/png',
+                        'density' => $density ? $density : null
+                    ];
+                }
+
+                unset( $json['icon'] );
+                unset( $json['sizes'] );
+
+                return $json;
+            }
+        ],
+        [
             'pattern' => 'browserconfig.xml',
             'action'  => function () {
 
-                $icon = asset( option('moritzebeling.kirby-favicon.icons.ms-tile') );
-                $icon = $icon->exists() ? $icon : asset( option('moritzebeling.kirby-favicon.icons.png') );
+                $icons = [
+                    option('moritzebeling.kirby-favicon.browserconfig.icon'),
+                    option('moritzebeling.kirby-favicon.favicon.png')
+                ];
+                foreach( $icons as $i ){
+                    $icon = asset( $i );
+                    if( $icon->exists() ) break;
+                }
 
-                $content = snippet('favicon/browserconfig', [
-                    'sizes' => $icon->exists() ? [
-                        70,
-                        150,
-                        310
-                    ] : [],
+                $xml = snippet('favicon/browserconfig', [
+                    'sizes' => $icon->exists() ? option('moritzebeling.kirby-favicon.browserconfig.sizes', []) : [],
                     'icon' => $icon,
                     'color' => option('moritzebeling.kirby-favicon.color'),
                 ], true);
 
-                return new Response($content, 'application/xml');
+                return new Response($xml, 'application/xml');
             }
         ]
     ]
