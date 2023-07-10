d0279d1ad35c25fd587e45370cde03d1aea0e160
moritzebeling <git@moritzebeling.com>
2023-07-10 16:10:32 +0200

    fix mediaUrl for SVGs

--- snippets/favicon.php
+++ snippets/favicon.php
@@ -9,13 +9,12 @@ $app = asset( option('moritzebeling.kirby-favicon.app.icon') );
 $mask = asset( option('moritzebeling.kirby-favicon.mask') );
 $color = option('moritzebeling.kirby-favicon.color');
 
-$tags = $tags ?? [];
-
 /* svg */
 
 if( $svg->exists() ){
-    $url = $svg->mediaUrl();
-    $tags[] = "<link rel='icon' type='image/svg+xml' href='$url'>";
+
+    $url = $svg->url();
+    echo "<link rel='icon' type='image/svg+xml' href='$url'>";
 }
     
 /* png */
@@ -26,44 +25,55 @@ if( $png->exists() ){
         if( $minimalist || $size === 180 ){ continue; }
         $url = $png->resize( $size )->url();
         $s = $size . 'x' . $size;
-        $tags[] = "<link rel='icon' type='image/png' sizes='$s' href='$url'>";
+        echo "<link rel='icon' type='image/png' sizes='$s' href='$url'>";
     }
 
     $url = $png->resize( 180 )->url();
-    $tags[] = "<link rel='alternate icon' type='image/png' href='$url'>";
+    echo "<link rel='alternate icon' type='image/png' href='$url'>";
 
 }
 
 /* apple touch icon */
 
-$app = $app->exists() ? $app : $svg;
 if( $app->exists() ){
 
     $url = $png->resize( 180 )->url();
-    $tags[] = "<link rel='apple-touch-icon' type='image/png' href='$url'>";
+    echo "<link rel='apple-touch-icon' type='image/png' href='$url'>";
     
     foreach( option('moritzebeling.kirby-favicon.app.sizes', []) as $size ){
         if( $minimalist || $size === 180 ){ continue; }
         $url = $png->resize( $size )->url();
         $s = $size . 'x' . $size;
-        $tags[] = "<link rel='apple-touch-icon' type='image/png' sizes='$s' href='$url'>";
+        echo "<link rel='apple-touch-icon' type='image/png' sizes='$s' href='$url'>";
     }
+
+} else if( $svg->exists() ){
+
+    $url = $svg->url();
+    echo "<link rel='apple-touch-icon' type='image/svg+xml' href='$url'>";
 
 }
 
 /* theme color */
 
 if( $color ){
-    $tags[] = "<meta name='theme-color' content='$color'>";
+    echo "<meta name='theme-color' content='$color'>";
 }
 
 /* safari pinned tab */
 
-$mask = $mask->exists() ? $mask : $svg;
 if( $mask->exists() ){
+
     $url = $mask->mediaUrl();
     $c = $color ?? '#ffffff';
-    $tags[] = "<link rel='mask-icon' href='$url' color='$c'>";
+    echo "<link rel='mask-icon' href='$url' color='$c'>";
+    
+} else if( $svg->exists() ){
+    
+    $url = $svg->url();
+    $c = $color ?? '#ffffff';
+    echo "<link rel='mask-icon' href='$url' color='$c'>";
+
 }
 
 /*
@@ -72,23 +82,19 @@ extended
     browserconfig.xml
 */
 
-if( $extended ){
+if( !$extended ){ return; }
 
-    $tags[] = "<link rel='manifest' href='/manifest.json'>";
-    $tags[] = "<meta name='msapplication-config' content='/browserconfig.xml'>";
+echo "<link rel='manifest' href='/manifest.json'>";
+echo "<meta name='msapplication-config' content='/browserconfig.xml'>";
 
-    if( $color ){
-        $tags[] = "<meta name='msapplication-TileColor' content='$color'>";
-    }
-
-    $tile = asset( option('moritzebeling.kirby-favicon.browserconfig.icon') );
-    $tile = $tile->exists() ? $tile : $png;
-
-    if( $tile->exists() ){
-        $url = $tile->resize( 144 )->url();
-        $tags[] = "<meta name='msapplication-TileImage' content='$url'>";
-    }
-
+if( $color ){
+    echo "<meta name='msapplication-TileColor' content='$color'>";
 }
 
-echo implode('',$tags);
+$tile = asset( option('moritzebeling.kirby-favicon.browserconfig.icon') );
+$tile = $tile->exists() ? $tile : $png;
+
+if( $tile->exists() ){
+    $url = $tile->resize( 144 )->url();
+    echo "<meta name='msapplication-TileImage' content='$url'>";
+}
